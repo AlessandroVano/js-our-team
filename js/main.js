d3d85b66f25d410c5aa62e34f794225192143f1f
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-11-04 17:47:17 +0100

    creazione arrey nuovi membri da inserire nel bottone,creazione funzione per inserirli ad ogni click

--- js/main.js
+++ js/main.js
@@ -8,8 +8,17 @@ Inserire a inizio del file JavaScript una sezione di commento dove scrivere gli 
 
 
 // processi logici 
+
+//  STAMPAGGIO CARD DEL NOSTRO TEAM
+
 // 1. creare un array di oggetti inserendo l'immagine, il titolo e la descrizione
 // 2. richiamare la classe contenitore e creare una funzione con un ciclo for al suo interno per dirgli di creare le altre immagini con nome e ruolo e dirgli di lupparle tutte in sequenza
+
+
+//   STAMPAGGIO NUOVI UTENTI
+// 3.  A) creazione arrey nuovi membri da inserire nel bottone, creazione funzione per inserirli ad ogni click
+
+
 
 
 // 1.
@@ -58,29 +67,73 @@ const membri = [
 
 
 
+
+
 // 2. 
 
 const teamContainer = document.querySelector('.team-container');
 
+// 3.
+const bottone = document.getElementById('addMemberButton');
+const nuovoUtenteNome = document.getElementById('name');
+const nuovoUtenteRuolo = document.getElementById('role');
+const nuovoUtenteFoto = document.getElementById('image');
+   
+
+
+
+
 // 2. b)
-generaMembri(membri, teamContainer);
+ generaMembri(membri, teamContainer); 
+
+bottone.addEventListener('click', () => {
 
 
+    const nuoviMembri = [ 
+        {
+        nome: 'Michelle Obama',
+        ruolo: 'pubbliche relazioni',
+        foto:  'img/new-team-member-01.jpg',
+
+    },
+    {
+        nome: 'Michelle Obama',
+        ruolo: 'pubbliche relazioni',
+        foto:  'img/new-team-member-02.jpg',
+
+    },
+    {
+        nome: 'Michelle Obama',
+        ruolo: 'pubbliche relazioni',
+        foto:  'img/new-team-member-03.jpg',
+
+    },
+    {
+        nome: 'Michelle Obama',
+        ruolo: 'pubbliche relazioni',
+        foto:  'img/new-team-member-04.jpg',
+
+    }
+]
+
+     membri.push(nuoviMembri);
+     console.log(membri);
+
+     generazioneNuoviMembri(nuoviMembri, teamContainer)
+     
+});
 
 
 
 
 //2. a)
-
+ /* FUNZIONE STAMPAGGIO NOSTRO TEAM */
 function generaMembri(membri, teamContainer) {
     for(let i = 0; i < membri.length; i++) {
         const membriItem = membri[i];
 
     console.log(membriItem);
-    
-    
 
-    // 4 creare e aggiungere il markp dei post
      teamContainer.innerHTML += `
      <div class="team-card">
             <div class="card-image">
@@ -97,3 +150,27 @@ function generaMembri(membri, teamContainer) {
       </div>`;
     }
 }
+
+// 3 a)
+/* FUNZIONE STAMPAGGIO NUOVI MEMBRI */
+ function generazioneNuoviMembri(nuoviMembri, teamContainer) {
+    for(let i = 0; i < nuoviMembri.length; i++) {
+    teamContainer.innerHTML +=`
+    <div class="team-card">
+    <div class="card-image">
+    <img
+     src="${nuoviMembri[i].foto}"
+      
+      />
+    </div>
+    <div class="card-text">
+    <h3>${nuoviMembri[i].nome}</h3>
+    <p>${nuoviMembri[i].ruolo}</p>
+    </div>
+  </div>
+</div>
+</div>`;
+    }
+} 
+
+
